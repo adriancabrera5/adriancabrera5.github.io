8bbd3dadecfec8093e6eac5ad4f78e2e98f06ed1
Adrian Cabrera <acc202@miami.edu>
2018-12-10 22:58:20 -0500

    Website

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     $(".text-column").addClass("animate");
+    setTimeout(function(){
+      $("img").addClass("animate");
+    },1000);
 });
